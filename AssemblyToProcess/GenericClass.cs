603e0d7f863bdc7fa1e01a6692fc0a6a9628e47b
Rafał Jasica <rafal@jasica.net>
2013-09-14 14:11:10 +0200

    Update GenericClass.cs
    remove test code

--- AssemblyToProcess/GenericClass.cs
+++ AssemblyToProcess/GenericClass.cs
@@ -25,15 +25,6 @@ public class GenericClass<T> where T : GenericClassBaseClass
     }
 
     public IEnumerable<T> B { get; set; }
-
-
-    public string ToString2323232()
-      {
-        return string.Format((IFormatProvider) CultureInfo.InvariantCulture, "{{T: \"GenericClass`1\", A: {0}}}", new object[1]
-        {
-          (object) this.A
-        });
-      }
 }
 
 [ToString]
