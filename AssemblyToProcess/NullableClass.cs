9668739709c9859bcd178fced27be8b6d35bf1c6
Simon Cropp <simon.cropp@gmail.com>
2013-09-30 13:41:39 +1000

    simplify projects

---
+++ AssemblyToProcess/NullableClass.cs
@@ -0,0 +1,11 @@
+﻿[ToString]
+public class NullableClass
+{
+    public int? X { get; set; }
+
+    public string Y { get; set; }
+
+    public double? Z { get; set; }
+
+    public char? V { get; set; }
+}
