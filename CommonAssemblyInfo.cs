8c15c5c2296f4d64a74977e7c115abe0437e2a13
rjasica <rafal@jasica.net>
2014-01-07 20:59:55 +0100

    update assembly version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("Foody.ToString")]
 [assembly: AssemblyProduct("Foody.ToString")]
-[assembly: AssemblyVersion("1.5.2.0")]
-[assembly: AssemblyFileVersion("1.5.2.0")]
+[assembly: AssemblyVersion("1.5.3.0")]
+[assembly: AssemblyFileVersion("1.5.3.0")]
