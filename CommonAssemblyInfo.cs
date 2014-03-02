640381973738cf6bcb3e3a3ae4cade57f64e0fe2
Simon Cropp <simon.cropp@gmail.com>
2014-03-02 11:25:52 +1100

    dev dep

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("Foody.ToString")]
 [assembly: AssemblyProduct("Foody.ToString")]
-[assembly: AssemblyVersion("1.5.5.0")]
-[assembly: AssemblyFileVersion("1.5.5.0")]
+[assembly: AssemblyVersion("1.5.6")]
+[assembly: AssemblyFileVersion("1.5.6")]
