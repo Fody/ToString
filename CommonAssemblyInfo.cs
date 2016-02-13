880824b6668349280ee283ec40c9768feb68df27
Simon Cropp <simon.cropp@gmail.com>
2016-02-13 23:07:26 +1100

    version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("Foody.ToString")]
 [assembly: AssemblyProduct("Foody.ToString")]
-[assembly: AssemblyVersion("1.7.1")]
-[assembly: AssemblyFileVersion("1.7.1")]
+[assembly: AssemblyVersion("1.7.2")]
+[assembly: AssemblyFileVersion("1.7.2")]
